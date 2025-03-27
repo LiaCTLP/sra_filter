fc1f180437e2e8344c78352e0781512bf25cb2a8
LiaCTLP <lia.costa@temple-legal.co.uk>
2025-03-27 12:46:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -36,15 +36,6 @@ def fetch_sra_companies():
     else:
         return []
 
-def authenticate_api_key(api_key):
-    return Keys_db.get(api_key)
-
-
-@app.before_request
-def before_request():
-    api_key = request.headers.get('API-Key')
-    if not api_key or not authenticate_api_key(api_key):
-        return jsonify({'error': 'Unauthorized'}), 401
 
 @app.route('/filtered_companies', methods=['GET'])
 def get_filtered_companies():
